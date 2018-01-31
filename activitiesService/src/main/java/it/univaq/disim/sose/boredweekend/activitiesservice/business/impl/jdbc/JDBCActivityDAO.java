72d7abb368f52ef83693bb78cbfaae713dfac89b
Vincenzo Battisti <vincenzo.battisti@gmail.com>
2018-01-31 12:47:15 +0100

    aggiunta funzione find (in activity service) ultimata

--- activitiesService/src/main/java/it/univaq/disim/sose/boredweekend/activitiesservice/business/impl/jdbc/JDBCActivityDAO.java
+++ activitiesService/src/main/java/it/univaq/disim/sose/boredweekend/activitiesservice/business/impl/jdbc/JDBCActivityDAO.java
@@ -6,17 +6,13 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
 import javax.sql.DataSource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import it.univaq.disim.sose.boredweekend.activitiesservice.ActivityCategory;
 import it.univaq.disim.sose.boredweekend.activitiesservice.Daytime;
 import it.univaq.disim.sose.boredweekend.activitiesservice.WeekDay;
@@ -38,16 +34,15 @@ public class JDBCActivityDAO implements ActivityDAO {
 	private static final String INFO_COLUMN = "info";
 	private static final String PAYMENT_COLUMN = "payment";
 	private static final String IMG_COLUMN = "img";
- 	
+	
+	// nomi colonne tabella category
 	private static final String FK_ACTIVITIES_ID_CATEGORY = "id_activity";
 	private static final String CATEGORY_COLUMN = "category";
 	
+	// nomi colonne tabella day
 	private static final String FK_ACTIVITIES_ID_DAYS ="id_activity";
 	private static final String DAY_COLUMN = "day";
 
-	
-	
-	
 	@Autowired
 	private DataSource dataSource;
 
@@ -65,107 +60,85 @@ public class JDBCActivityDAO implements ActivityDAO {
 	@Override
 	public List<Activity> find(String city, List<ActivityCategory> categories, List<WeekDay> days, Daytime daytime) {
 
+		// inizio costruzione query
 		String sql_init = "SELECT a.*, b."+DAY_COLUMN+", c."+CATEGORY_COLUMN+" FROM activities as a JOIN activities_days as b JOIN category_type as c ON a."+ID_COLUMN+"=b."+FK_ACTIVITIES_ID_DAYS+" AND a."+ID_COLUMN+"=c."+FK_ACTIVITIES_ID_CATEGORY+" WHERE a."+CITY_COLUMN+" ='"+city+"'";	
 		
 		if(!days.isEmpty()) {
-
-			sql_init += " AND (";
-					
+			sql_init += " AND (";		
 			String sql_day = sql_init;
-			
 			for(int i =0; i<days.size();i++) {
 				sql_day = sql_day+"b."+DAY_COLUMN+"='"+days.get(i)+"' OR ";
 			}
-			
-			sql_init = sql_day.substring(0, sql_day.length()-4)+")";
-									
+			sql_init = sql_day.substring(0, sql_day.length()-4)+")";						
 		}
 		
 		if(!categories.isEmpty()) {
-			
 			sql_init += " AND (";
-		
-		    
 			String sql_category = sql_init;
-			
 			for(int i =0; i<categories.size();i++) {
 				sql_category = sql_category+"c."+CATEGORY_COLUMN+"='"+categories.get(i)+"' OR ";
 			}
-			
 			sql_init = sql_category.substring(0, sql_category.length()-4)+")";
-			
 		}
 		
+		//sql_init contiene la query del giusto formato
 		sql_init = sql_init+" AND a.daytime='"+daytime.value()+"'";
 		
-		//sql_init contiene la query del giusto formato
-		
-		
-		String sql = "SELECT * FROM activities WHERE " + CITY_COLUMN + "=\"" + city + "\" AND " + DAYTIME_COLUMN + "=\"" + daytime.value() + "\"";
-		
-		//SELECT * FROM activities as a JOIN activities_days ON a.id_activity=activities_days.id_activity
-
 		LOGGER.info("Activity DAO is going to perform the query: " + sql_init);
-		
-        
 	
 		List<Activity> result = new ArrayList<>();
 		Connection con = null;
 		Statement st = null;
 		ResultSet rs = null;
-		
-		/*
-		ResultSet result_day = null;
-		ResultSet result_category = null;
-		*/
-	    
-
-		
+		// utilizziamo questa map per controllare che l'attività non sia già stata inserita nel result
 		Map<Integer, Activity> activityMap = new HashMap<>();
-		List<Activity> activityList = null;
-
-		
+		//liste di map che rispettivamente conterranno giorni e categorie delle righe del result set
+		List<Map<Integer,String>> listMap = new ArrayList<>();
+		List<Map<Integer,String>> listMap2 = new ArrayList<>(); 
 		
 		try {
 			con = dataSource.getConnection();
 			st = con.createStatement();
-			rs = st.executeQuery(sql_init);		
+			rs = st.executeQuery(sql_init);
 			
+			// popolazione della lista di giorni e categorie
+			while(rs.next()) {
+				Map<Integer,String> mapDay = new HashMap<>();
+				Map<Integer,String> mapCategory = new HashMap<>();
+				mapDay.put(rs.getInt(ID_COLUMN), rs.getString(DAY_COLUMN));
+				listMap.add(mapDay);
+				mapCategory.put(rs.getInt(ID_COLUMN), rs.getString(CATEGORY_COLUMN));
+				listMap2.add(mapCategory);
+			}
+			
+			// serve per far ripuntare alla prima locazione del result set 
+			rs.beforeFirst();
+			
+			// popolazione della lista di attività da ritornare
 			while (rs.next()) {
-				
-				/*
+				// controllo che l'attività con quell'id non sia già stata inserita, se si scarto la riga
 				if(!activityMap.containsKey(rs.getInt(ID_COLUMN))) {
-					System.err.println("diocristo2");
-					
 					
 					List<WeekDay> day_list = new ArrayList<WeekDay>();
-					
 					List<ActivityCategory> category_list = new ArrayList<ActivityCategory>();
 					
-					
-					
-					while(result_day.next()) {
-						
-						if(result_day.getInt(ID_COLUMN) == rs.getInt(ID_COLUMN) && !day_list.contains(result_day.getObject(DAY_COLUMN))){
-							System.err.println("asdasfsjkdh");
-							day_list.add((WeekDay) result_day.getObject(DAY_COLUMN));
+					for(Map<Integer,String> m : listMap) {
+						if(m.containsKey(rs.getInt(ID_COLUMN)) && !day_list.contains(WeekDay.fromValue(m.get(rs.getInt(ID_COLUMN))))) {
+							WeekDay w = WeekDay.fromValue(m.get(rs.getInt(ID_COLUMN)));
+							day_list.add(w);
 						}
 					}
-					System.err.println(day_list);
 					
-					
-					System.err.println(day_list);
-					
-					while(result_category.next()) {
-						if(result_category.getInt(ID_COLUMN) == rs.getInt(ID_COLUMN) && !category_list.contains(result_category.getObject(CATEGORY_COLUMN))){
-							category_list.add((ActivityCategory)result_category.getObject(CATEGORY_COLUMN));
+					for(Map<Integer,String> m : listMap2) {
+						if(m.containsKey(rs.getInt(ID_COLUMN)) && !category_list.contains(ActivityCategory.fromValue(m.get(rs.getInt(ID_COLUMN))))) {
+							ActivityCategory a = ActivityCategory.fromValue(m.get(rs.getInt(ID_COLUMN)));
+							category_list.add(a);
 						}
 					}
 					
 					System.err.println(category_list);
-
-
-					/*
+					System.err.println(day_list);
+				
 					Activity activity = new Activity();
 					
 					activity.setId(rs.getInt(ID_COLUMN));
@@ -183,31 +156,11 @@ public class JDBCActivityDAO implements ActivityDAO {
 					activityMap.put(rs.getInt(ID_COLUMN), activity);
 					
 					result.add(activity);
-					
+				
 				}
-				*/
-				
-				Activity activity = new Activity();
-				
-				activity.setId(rs.getInt(ID_COLUMN));
-				activity.setName(rs.getString(NAME_COLUMN));
-				activity.setCity(rs.getString(CITY_COLUMN));
-				activity.setLat(rs.getLong(LAT_COLUMN));
-				activity.setLat(rs.getLong(LON_COLUMN));
-				activity.setDaytime(Daytime.fromValue(rs.getString(DAYTIME_COLUMN)));
-				activity.setState(rs.getBoolean(STATE_COLUMN));
-				activity.setInfo(rs.getString(INFO_COLUMN));
-				activity.setPayment(rs.getBoolean(PAYMENT_COLUMN));
-				activity.setImg(rs.getBytes(IMG_COLUMN));
-
-				result.add(activity);
-
 				
 			}
 			
-			activityList = new ArrayList<Activity>(activityMap.values());
-			
-
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
